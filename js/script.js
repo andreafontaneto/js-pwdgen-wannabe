b35a7360fde9c5a14c54cbc0003f10e3d61d8e90
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-20 13:57:08 +0200

    consegna esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('ciao');
+// creare un generatore di password 
+
+/*
+
+chiedendo all'utente:
+
+1- nome
+2- cognome
+3- colore preferito
+4- stampare la password generata
+
+es. nomecognomecolorepreferito21
+
+*bonus*
+5- stampare la lunghezza della password 
+
+*/
